e22aeb1619866563371b05875735f0f547eb478d
Racque Cordeta <66395124+Ghostcoder0427@users.noreply.github.com>
2024-09-27 13:21:59 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import "./bootstrap";
+// resources/js/app.js
+import "bootstrap";
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import { createApp, defineAsyncComponent } from "vue/dist/vue.esm-bundler.js";
